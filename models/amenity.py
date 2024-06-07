8f227ac64b8e4c4bbba39bec620eaaa81c31a7c7
NGONORMALIA <maliangonor00@gmail.com>
2024-06-07 12:57:15 -0700

    Airbnb project

---
+++ models/amenity.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""the Amenity class."""
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """an amenity.
+    Attributes:
+        name (str): The name of the amenity.
+    """
+
+    name = ""
